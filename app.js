6bb4610ecb97be7e04778a81da13806b4ccd9a34
jokinsey <jokinsey@email.uark.edu>
2017-08-02 00:06:01 -0500

    Add functionality for the Profile and Authenticate routes

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const express = require ('express');
 const path = require ('path');
 const bodyParser = require ('body-parser');
 const cors = require ('cors');
-const passport = require ('passport-jwt');
+const passport = require ('passport');
 const mongoose = require ('mongoose');
 const config = require('./config/database');
 
+//Connect to Database
 mongoose.connect(config.database);
 
+//On Connection
 mongoose.connection.on('connected', function(){
   console.log('Connected to database'+ config.database);
+});
+
+//On Error
+mongoose.connection.on('error', function(err){
+  console.log('Database error: '+err);
 });
 
 const app = express();
@@ -21,6 +28,12 @@ app.use(cors());
 
 // Body Parser Middleware
 app.use(bodyParser.json());
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
 
 //Port Number
 const port = 3000;
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/user');
 const passport = require("passport");
 const jwt = require('jsonwebtoken');
+const config = require('../config/database');
 
 //Register
 router.post('/register', function(req, res, next){
@@ -23,13 +24,45 @@ router.post('/register', function(req, res, next){
 });
 
 //Profile
-router.get('/profile', function(req, res, next){
-  res.send('PROFILE');
+router.get('/profile', passport.authenticate('jwt', {session:false}), function(req, res, next){
+  res.json({
+    user: req.user
+  });
 });
 
 //Authentication
 router.post('/authenticate', function(req, res, next){
-  res.send('AUTHENTICATE');
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.getUserByUsername(username, function(err, user){
+    if(err) throw err;
+    if(!user){
+      return res.json({success:false, msg:'User not found'});
+    }
+
+    User.comparePassword(password, user.password, function(err, isMatch){
+      if(err) throw err;
+      if(isMatch){
+        const token = jwt.sign(user, config.secret, {
+          expiresIn: 604800 // 1 week
+        });
+
+        res.json({
+          success:true,
+          token: 'JWT' + token,
+          user:{
+            id: user._id,
+            name: user.name,
+            username: user.username,
+            email: user.email
+          }
+        });
+      } else {
+        return res.json({success:false, msg:'Wrong password'});
+      }
+    });
+  });
 });
 
 
